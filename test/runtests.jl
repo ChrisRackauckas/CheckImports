92d1eea41543ed82bef2ad6faeaa67bcf5154cb5
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-05-30 00:45:06 -0700

    Change test to use REQUIRE

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,5 @@ using Base.Test
 println(modulechildren(Main))
 @test isimported("CheckImports")
 @test !isimported("BenchmarkLite",m=Main)
-Pkg.add("BenchmarkLite")
 using BenchmarkLite
 @test isimported("BenchmarkLite",m=Main)
